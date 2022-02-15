30f0f0b5c1bbe6ac3a89e770bbe6de83ddb259f0
Su Sun <56568705+ssun30@users.noreply.github.com>
2022-02-15 13:33:39 -0500

    Delete interfaces/Matlab_Toolbox_Revamp/Test Folder directory

--- interfaces/Matlab_Toolbox_Revamp/Test Folder/testclassA.m
+++
@@ -1,8 +0,0 @@
-classdef testclassA < handle & testsuperclassA & testsuperclassB
-    methods
-        function t = testclassA(temperature, pressure, flowrate, density)
-            t@testsuperclassA(temperature, pressure);
-            t@testsuperclassB(flowrate, density);
-        end
-    end
-end
