dd532259ba9568db1b1313c4c8909d50c4eae7e2
ssun30 <sun.su@northeastern.edu>
2022-06-17 12:54:04 -0400

    SCON Install Experiment

---
+++ interfaces/MatlabToolbox/matlab_legacy/Contents.m
@@ -0,0 +1,9 @@
+% Cantera Toolbox
+% Version 2.6.0a4
+%
+% Cantera is a suite of object-oriented software tools for
+% problems involving chemical kinetics, thermodynamics,
+% and/or transport processes.
+%
+% Documentation for Cantera can be found at
+% https://cantera.org/documentation/index.html
