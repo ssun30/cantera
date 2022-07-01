15124e43756f9719db449b7012c32a85e2fd7ea6
ssun30 <sun.su@northeastern.edu>
2022-06-30 22:09:18 -0400

    fixed some installation text inconsistencies

--- interfaces/MatlabToolbox/matlab_new/Utility/cleanup.m
+++ interfaces/MatlabToolbox/matlab_new/Utility/cleanup.m
@@ -4,7 +4,6 @@ function cleanup()
     checklib;
     calllib(ct, 'ct_clearOneDim');
     calllib(ct, 'ct_clearMix');
-    calllib(ct, 'ct_clearXML');
     calllib(ct, 'ct_clearFunc');
     calllib(ct, 'ct_clearStorage');
     calllib(ct, 'ct_clearReactors');
