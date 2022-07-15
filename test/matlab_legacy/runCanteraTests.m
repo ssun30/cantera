a66f33d66a9a06f6f466d5483b5975b3574c9377
ssun30 <sun.su@northeastern.edu>
2022-07-15 13:10:42 -0400

    Incorporate new toolbox into scons test

--- test/matlab_legacy/runCanteraTests.m
+++ test/matlab_legacy/runCanteraTests.m
@@ -1,6 +1,6 @@
 function  runCanteraTests(varargin)
 % Set Cantera paths
-run ../../interfaces/matlab/Utility/testpath.m
+run ../../interfaces/matlab_legacy/testpath.m
 
 % Set test framework path
 path('../../ext/matlab_xunit', path)
