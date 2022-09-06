db99e32647bc590715e5adbbbba013ce28952615
ssun30 <sun.su@northeastern.edu>
2022-09-06 14:32:37 -0400

    Revert SConscript for building Matlab Toolbox and Matlab Tests

--- test/matlab/runCanteraTests.m
+++ test/matlab/runCanteraTests.m
@@ -1,6 +1,6 @@
 function  runCanteraTests(varargin)
 % Set Cantera paths
-run ../../interfaces/matlab_legacy/testpath.m
+run ../../interfaces/matlab/testpath.m
 
 % Set test framework path
 path('../../ext/matlab_xunit', path)
